fac79b54be68d64a1002090585727796a2fac565
damian.trinca <damian.trinca>
2024-05-06 10:19:12 -0300

    Feature: Add SpAnalizer

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ function processFile(filePath: string, fileName: string) {
 
             const data = fs.readFileSync(filePath, 'utf8');
             if (data.length > 0) {
-                const targetSubstring = "GetStoredProcCommand";
+                const targetSubstring = "origen_cliente";
                 if (data.includes(targetSubstring)) {
                     // myLogger.log({ filePath })
                     const file = filePath.split('//').reverse()[0];
@@ -42,6 +42,7 @@ function processFile(filePath: string, fileName: string) {
     }
 }
 
+
 function traverseDirectory(dir: string, fileName: string) {
     try {
         const files = fs.readdirSync(dir);
@@ -56,102 +57,107 @@ function traverseDirectory(dir: string, fileName: string) {
 
 //#region Global Var
 
-var projects = [
-    'C:/Dev/Efecty.SiewebLive',
-    'C:/Dev/Efecty.CB.DispersionPagos',
-    'C:/Dev/Efecty.CB.Monitor',
-    'C:/Dev/Efecty.CB.Service',
-    'C:/Dev/Efecty.CB.Socket',
-    'C:/Dev/Efecty.CB.Transacciones',
-    "C:/Dev/Efecty.Facturacion.Ingestion",
-    "C:/Dev/Efecty.Microservicios.Antifraude",
-    "C:/Dev/Efecty.Microservicios.Billetera",
-    "C:/Dev/Efecty.Microservicios.Biometria",
-    "C:/Dev/Efecty.Microservicios.Broker",
-    "C:/Dev/Efecty.Microservicios.Contabilidad",
-    "C:/Dev/Efecty.Microservicios.Legados",
-    "C:/Dev/Efecty.Microservicios.Plataforma.NET_6",
-    "C:/Dev/Efecty.Microservicios.Seguridad",
-    "C:/Dev/Efecty.Microservicios.Transaccion",
-    "C:/Dev/Efecty.Microservicios.YAML",
-    "C:/Dev/Efecty.ServicioBiometriaMorpho",
-    "C:/Dev/Efecty.SiewebLive",
-    "C:/Dev/Efecty.SiewebLive.Integraciones",
-    "C:/Dev/Efecty.SiewebLive.PosCache",
-    "C:/Dev/Efecty.WebLive.Administracion",
-    "C:/Dev/Efecty.WebLive.ClienteEmpresarial",
-    "C:/Dev/Efecty.WebLive.Contratistas",
-    "C:/Dev/Efecty.WebLive.Impuestos",
-    "C:/Dev/Efecty.WebLive.Interfaces",
-    "C:/Dev/Efecty.WebLive.Main",
-    "C:/Dev/Efecty.WebLive.Presupuesto",
-    "C:/Dev/Efecty.WebLive.ReportesHuellaPago",
-    "C:/Dev/Efecty.WebLive.Tarificador",
-    "C:/Dev/Efecty.WebLive20",
-];
+function init() {
+    var projects = [
+        'C:/Dev/Efecty.SiewebLive',
+        'C:/Dev/Efecty.CB.DispersionPagos',
+        'C:/Dev/Efecty.CB.Monitor',
+        'C:/Dev/Efecty.CB.Service',
+        'C:/Dev/Efecty.CB.Socket',
+        'C:/Dev/Efecty.CB.Transacciones',
+        "C:/Dev/Efecty.Facturacion.Ingestion",
+        "C:/Dev/Efecty.Microservicios.Antifraude",
+        "C:/Dev/Efecty.Microservicios.Billetera",
+        "C:/Dev/Efecty.Microservicios.Biometria",
+        "C:/Dev/Efecty.Microservicios.Broker",
+        "C:/Dev/Efecty.Microservicios.Contabilidad",
+        "C:/Dev/Efecty.Microservicios.Legados",
+        "C:/Dev/Efecty.Microservicios.Plataforma.NET_6",
+        "C:/Dev/Efecty.Microservicios.Seguridad",
+        "C:/Dev/Efecty.Microservicios.Transaccion",
+        "C:/Dev/Efecty.Microservicios.YAML",
+        "C:/Dev/Efecty.ServicioBiometriaMorpho",
+        "C:/Dev/Efecty.SiewebLive",
+        "C:/Dev/Efecty.SiewebLive.Integraciones",
+        "C:/Dev/Efecty.SiewebLive.PosCache",
+        "C:/Dev/Efecty.WebLive.Administracion",
+        "C:/Dev/Efecty.WebLive.ClienteEmpresarial",
+        "C:/Dev/Efecty.WebLive.Contratistas",
+        "C:/Dev/Efecty.WebLive.Impuestos",
+        "C:/Dev/Efecty.WebLive.Interfaces",
+        "C:/Dev/Efecty.WebLive.Main",
+        "C:/Dev/Efecty.WebLive.Presupuesto",
+        "C:/Dev/Efecty.WebLive.ReportesHuellaPago",
+        "C:/Dev/Efecty.WebLive.Tarificador",
+        "C:/Dev/Efecty.WebLive20",
+    ];
 
-let globalStoreProcedures: { file: string; storedProcedureName: string }[] = [];
-let globalCount = 0;
-let globalFileName = 'global-store-procedures';
+    let globalStoreProcedures: { file: string; storedProcedureName: string }[] = [];
+    let globalCount = 0;
+    let globalFileName = 'global-store-procedures';
 
-//#endregion
+    //#endregion
 
-//#region Local to the projects
-let count = 0;
-let storeProceduresFound: { file: string; storedProcedureName: string }[] = [];
+    //#region Local to the projects
 
-let myLogger = new Console({
-    stdout: fs.createWriteStream(`files/stdout.txt`), // Save normal logs here
-    stderr: fs.createWriteStream("files/errStdErr.txt"),    // Save error logs here
-});
 
-projects.forEach(project => {
-    count = 0;
-    storeProceduresFound = [];
-    const directoryPath: string = project;
-    const fileName = directoryPath.replaceAll(':', '').replaceAll('/', '-').replaceAll('.', '');
-
-    myLogger = new Console({
-        stdout: fs.createWriteStream(`files/${fileName}.txt`),
-        stderr: fs.createWriteStream("files/errStdErr.txt"),
+    let myLogger = new Console({
+        stdout: fs.createWriteStream(`files/stdout.txt`), // Save normal logs here
+        stderr: fs.createWriteStream("files/errStdErr.txt"),    // Save error logs here
     });
 
-    traverseDirectory(directoryPath, fileName);
+    projects.forEach(project => {
+        count = 0;
+        storeProceduresFound = [];
+        const directoryPath: string = project;
+        const fileName = directoryPath.replaceAll(':', '').replaceAll('/', '-').replaceAll('.', '');
 
-    csv.stringify(storeProceduresFound, (err, output) => {
+        myLogger = new Console({
+            stdout: fs.createWriteStream(`files/${fileName}.txt`),
+            stderr: fs.createWriteStream("files/errStdErr.txt"),
+        });
+
+        traverseDirectory(directoryPath, fileName);
+
+        csv.stringify(storeProceduresFound, (err, output) => {
+            if (err) {
+                console.error('Error creating CSV:', err);
+                return;
+            }
+            fs.writeFileSync(`files/${fileName}.csv`, output);
+        });
+
+        console.table(storeProceduresFound);
+        console.log({ fileName, count })
+        // myLogger.table({ count })
+
+        //Set the global
+        globalStoreProcedures = globalStoreProcedures.concat(storeProceduresFound);
+        globalCount = globalCount + count;
+    })
+
+    //#endregion
+
+    //#region Global SP
+
+    myLogger = new Console({
+        stdout: fs.createWriteStream(`files/${globalFileName}.txt`), // Save normal logs here
+        stderr: fs.createWriteStream("files/errStdErr.txt"),    // Save error logs here
+    });
+
+    csv.stringify(globalStoreProcedures, (err, output) => {
         if (err) {
             console.error('Error creating CSV:', err);
             return;
         }
-        fs.writeFileSync(`files/${fileName}.csv`, output);
+        fs.writeFileSync(`files/${globalFileName}.csv`, output);
     });
 
-    console.table(storeProceduresFound);
-    console.log({ fileName, count })
-    // myLogger.table({ count })
+    myLogger.table(globalStoreProcedures);
 
-    //Set the global
-    globalStoreProcedures = globalStoreProcedures.concat(storeProceduresFound);
-    globalCount = globalCount + count;
-})
-
-//#endregion
-
-//#region Global SP
-
-myLogger = new Console({
-    stdout: fs.createWriteStream(`files/${globalFileName}.txt`), // Save normal logs here
-    stderr: fs.createWriteStream("files/errStdErr.txt"),    // Save error logs here
-});
-
-csv.stringify(globalStoreProcedures, (err, output) => {
-    if (err) {
-        console.error('Error creating CSV:', err);
-        return;
-    }
-    fs.writeFileSync(`files/${globalFileName}.csv`, output);
-});
-
-myLogger.table(globalStoreProcedures);
-
-//#endregion
+    //#endregion
+}
+let count = 0;
+let storeProceduresFound: { file: string; storedProcedureName: string }[] = [];
+// console.log("Hey ma men")
+init();
---
+++ sp-analyzer.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var console_1 = require("console");
+var fs = require("fs");
+function openFile() {
+    var myLogger = new console_1.Console({
+        stdout: fs.createWriteStream("files/sp-analyzer.txt"), // Save normal logs here
+        stderr: fs.createWriteStream("files/sp-analyzerErrStd.txt"), // Save error logs here
+    });
+    var csvData = fs.readFileSync('C:/Users/DamiánTrinca/Downloads/Analisis DB(SpDefinitionDbClientes).csv', 'utf8').toString();
+    // Split the data into lines
+    var lines = csvData.split('\n');
+    // Initialize an empty array to store the parsed data
+    var dataArray = [];
+    // Process each line
+    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+        var line = lines_1[_i];
+        var values = line.split(';').map(function (value) { return value.trim(); });
+        var name_1 = values.shift();
+        dataArray.push({ name: name_1, sp: values.join(';') });
+    }
+    console.log({ dataArray: dataArray });
+    // const regex = /sp(?!\s)/; // Matches 'sp' not followed by a space
+    var data = dataArray.filter(function (data) { return data.sp.toLowerCase().includes('exec sp') || data.sp.toLowerCase().includes('exec  sp'); });
+    var filterSp = data.map(function (p) {
+        // const inputString = p.sp;
+        // const regex = /exec\s(\S+)/; // Matches 'exec ' followed by non-space characters
+        // const match = inputString.match(regex);
+        var result = p.sp.toLowerCase().split('exec ').filter(function (p) { return p.toLowerCase().includes('sp'); });
+        // .filter(p => p.toLowerCase().includes('sp'));
+        // myLogger.table(result)
+        return { name: p.name, result: result };
+        // const desiredSubstring = result[0].trimStart().split(' ')[0]
+        // console.log(desiredSubstring)
+        // return { sp: p.name, spfound: desiredSubstring ? desiredSubstring : '' };
+    });
+    var final = [];
+    filterSp.forEach(function (p) {
+        var newResult = [];
+        p.result.forEach(function (q) {
+            if (q.split(' ')[0].includes('sp')) {
+                newResult.push(q.split(' ')[0].replaceAll('[', '').replaceAll(']', ''));
+            }
+        });
+        if (newResult.length > 0) {
+            final.push({ name: p.name, sps: newResult });
+        }
+    });
+    var stringFinal = [];
+    final.forEach(function (p) {
+        p.sps.forEach(function (q) {
+            stringFinal.push("".concat(q, ",").concat(p.name));
+            myLogger.log("".concat(q, ",").concat(p.name));
+        });
+    });
+    var stringSet = new Set(stringFinal);
+    // myLogger.table(stringSet);
+}
+openFile();
+// const inputString = " -- =============================================  -- Author:  Orlando Tovar Valencia  -- Create date: 2019-03-22  -- Description: Consulta el usuario por identificacion y tipo de identificacion  -- =============================================  -- =============================================  -- Author:  Wilson Rene Camacho  -- Create date: 2019-05-23  -- Description: Se agregan tablas para traer la informacion del contratista(Razon social y correo).  -- =============================================  -- =============================================  -- Author:  Wilson Rene Camacho  -- Create date: 2019-05-28  -- Description: En el filtro tipo DIR tambien se agrega filtro tipo CON (WI-5747)  -- =============================================  -- =============================================    -- Author:  Fabian Baicue  -- Modified date: 2019-03-19  -- Description: se agrega campos de la tabla usuario: telefono y bandera (USUAutorizaDatos) de autorización tratamiento de datos.  -- WI: 4963   -- =============================================    -- Author   : Laura Yaneth Moreno Piñeros  -- Create date  : 23/01/2024  -- Description  : Se agregan campo USUHuePlantilla a consulta  -- Aplication  : SiewebLive  -- User Story US : 812  -- ===========================================================================================================================================  -- Author   : Laura Yaneth Moreno Piñeros  -- Create date  : 21/02/2024  -- Description  : Se elimina condicion PUNSERtipo IN ('DIR','CON') del campo EstaEnroladoPunDir  -- Aplication  : SiewebLive  -- User Story US : 1373  -- ===========================================================================================================================================  -- Author   : Cristian Camilo Urrego Rojas  -- Create date  : 22/02/2024  -- Description  : Se configuran los perfiles  ('CANCAJ','CANCAJPRI') en el parametro general,Se agrega el Join a USUARIOGRUPO  -- Aplication  : SiewebLive  -- User Story US : 1425  -- ===========================================================================================================================================  -- Eje: EXEC spUSUSelConsultaCajero '7732170','CC','010671'    CREATE PROCEDURE [dbo].[spUSUSelConsultaCajero]   @identificacion VARCHAR(15),@tipoIdentificacion CHAR(3),@codigoPAP VARCHAR(10)  AS  BEGIN   -- SET NOCOUNT ON added to prevent extra result sets from   -- interfering with SELECT statements.   SET NOCOUNT ON;      DECLARE @PerfilEnrrolamiento VARCHAR(50)   SELECT @PerfilEnrrolamiento = PARGENvalor   FROM PARAMETROS_GENERALES WITH(NOLOCK)    WHERE PARGENdescripcion = 'SiewebLive.POS.PerfilesEnrolamientoEspecialistas'        -- Insert statements for procedure here   SELECT    USUcodigo AS Codigo,USUnombre AS Nombre,USUapellidos AS Apellido,ISNULL(USUbloqueado,0) AS Bloqueado,USUcorreo AS Correo,USUenroladoPor AS EnroladoPor,USUidentificacion AS Identificacion,USUtipoIdentificacion AS TipoIdentificacion,ISNULL(USUConfronta,0) AS Confronta,convert(datetime,CASE WHEN USUConfrontaFecha IS NULL     THEN '01-01-1900 00:00:00'     ELSE USUConfrontaFecha    END,101) AS ConfrontaFecha,ISNULL(USUconfrontaReintentos,0) AS NoReintentos,CASE WHEN @codigoPAP = USUPS     THEN 1     ELSE 0    END AS EstaEnroladoMismoPAP,CASE WHEN (SELECT PUNSERcodigo FROM PUNTOSERVICIO WITH (NOLOCK) WHERE PUNSERcodigo = @codigoPAP AND PUNSERestado = 'ACT') IS NULL     THEN 1     ELSE 0    END AS EstaEnroladoPunDir,CASE WHEN (SELECT PUNSERIdRed FROM RED_CONTRATISTA WITH (NOLOCK) INNER JOIN PUNTOSERVICIO WITH (NOLOCK) ON PUNSERIdRed = REDCONId WHERE PUNSERcodigo = USUPS) =               (SELECT PUNSERIdRed FROM RED_CONTRATISTA WITH (NOLOCK) INNER JOIN PUNTOSERVICIO WITH (NOLOCK) ON PUNSERIdRed = REDCONId WHERE PUNSERcodigo = @codigoPAP)     THEN 1     ELSE 0    END AS EstaEnroladoPunRed,USUPS AS CodigoPAP,CONRazonsocial AS RazonSocialContratista,CONMail as CorreoContratista,USUNumTelefono as Telefono,USUAutorizaDatos as AutorizaDatos,CASE WHEN USUHuePlantilla IS NULL      THEN 0     ELSE 1    END AS EstaEnroladoPOS   FROM    USUARIO WITH(NOLOCK)    INNER JOIN PUNTOSERVICIO WITH(NOLOCK) ON      PUNSERcodigo = USUPS AND PUNSERestado = 'ACT'    INNER JOIN RED_CONTRATISTA WITH(NOLOCK) ON      PUNSERIdRed = REDCONId    INNER JOIN CONTRATISTA WITH(NOLOCK) ON      REDCONTipoIdContratista = CONtipoid and      REDCONIdContratista = CONidentificacion AND CONEstado = 'ACT'    INNER JOIN USUARIOGRUPO WITH(NOLOCK) ON      USUGRUusuario = USUcodigo AND      USUGRUgrupo IN (SELECT VALUE FROM STRING_SPLIT(@PerfilEnrrolamiento,','))   WHERE    USUtipoIdentificacion = @tipoIdentificacion AND    USUidentificacion = @identificacion AND     USUestado = 'ACT'    END";
+// let result = inputString.toLowerCase().split('exec').filter(p => p.toLowerCase().includes('sp'));
+// // console.log(result);
+// const desiredSubstring = result[0].trimStart().split(' ')[0]
+// console.log(desiredSubstring);
+// const inputString = 'exec  spCONREDPSSelConfiguracionRedPS CREATE...';
+// const regex = /exec\s(\S+)/; // Matches 'exec ' followed by non-space characters
+// const match = inputString.match(regex);
+// const desiredSubstring = match ? match[1] : ''; // Extract the captured group
+// console.log(desiredSubstring);
+// const myString = '/*Se modifica spp';
+// const regex = /sp(?!\s)/; // Matches 'sp' not followed by a space
+// const containsSP = regex.test(myString);
+// console.log(containsSP);
